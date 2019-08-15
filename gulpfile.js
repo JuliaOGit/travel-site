99e7f6d2315a15599d337898211ea4550a42dc44
Julia <j.ogorodniichuk@gmail.com>
2019-08-15 16:41:34 +0300

    Completed large hero block and intro content block.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-var gulp = require('gulp');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function () {
-    console.log("Hooray - you created a Gulp task.")
-});
 
-gulp.task('html', function () {
-    console.log("Imagine something useful being done to your HTML here.")
-});
+
+
+
+
+
+
+
+
